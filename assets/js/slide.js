ec44bf49a0a30aedb2215fc9f90784399c762e7d
HSinhz <vhieusinh2703@gmail.com>
2024-04-04 19:52:45 +0700

    update code front end

---
+++ assets/js/slide.js
@@ -0,0 +1,27 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll('.img-slider');
+
+function showSlide(index) {
+    slides.forEach(slide => {
+        slide.style.display = 'none';
+    });
+    slides[index].style.display = 'block';
+}
+
+function nextSlide() {
+    slideIndex++;
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+    showSlide(slideIndex);
+}
+
+function prevSlide() {
+    slideIndex--;
+    if (slideIndex < 0) {
+        slideIndex = slides.length - 1;
+    }
+    showSlide(slideIndex);
+}
+
+showSlide(slideIndex);
